a3e20757791aaefdbc447b4a9540baf49248027c
Grant Paton-Simpson <grant@sofastatistics.com>
2021-11-08 19:39:48 +1300

    Make ready for Heroku

--- phelp/run.py
+++ phelp/run.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request, jsonify
 
 from superhelp import conf
@@ -26,3 +28,9 @@ def get_help() -> str:
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 33507))
+    app.run(host='0.0.0.0', port=port)
+
