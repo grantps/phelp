6f876e17f1654332694c93c34e6a8d18b3fe3b54
Grant Paton-Simpson <grant@sofastatistics.com>
2021-11-08 19:46:40 +1300

    Fix port

--- phelp/run.py
+++ phelp/run.py
@@ -31,6 +31,6 @@ if __name__ == '__main__':
     
     
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 33507))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
